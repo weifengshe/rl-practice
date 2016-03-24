67d42300e097648d3937cc93bd6e7d29949e681d
Antti Tarvainen <antti.tarvainen@reaktor.com>
2016-03-24 12:21:38 +0200

    Use Markov decision process terminology

--- gridworld.py
+++ gridworld.py
@@ -2,28 +2,27 @@ import numpy as np
 import operator
 
 class GridWorld(object):
-  def __init__(self, dimensions, start, goal):
+  def __init__(self, dimensions, start_state, goal_state):
     self.dimensions = dimensions
-    self.current = start
-    self.goal = goal
-    self.all_positions = set(np.ndindex(dimensions))
+    self.current_state = start_state
+    self.goal_state = goal_state
+    self.states = set(np.ndindex(dimensions))
 
-  directions_with_names = [
+  actions_and_updates = [
     ('up', (-1, 0)),
     ('down', (1, 0)),
     ('left', (0, -1)),
     ('right', (0, 1))
   ]
 
-  directions = [direction for (_, direction) in directions_with_names]
-  direction_names = [name for (name, _) in directions_with_names]
+  actions = [name for (name, _) in actions_and_updates]
+  coordinate_updates = [direction for (_, direction) in actions_and_updates]
 
-  def move(self, direction_name):
-    def within_bounds(idx, size):
-      return max(0, min(idx, size - 1))
+  def take_action(self, action):
+    def keep_within_bounds(coordinate, dimension_size):
+      return max(0, min(coordinate, dimension_size - 1))
 
-    direction_idx = self.direction_names.index(direction_name)
-    coordinate_updates = self.directions[direction_idx]
-    updated_position = map(operator.add, self.current, coordinate_updates)
-    bounded_position = map(within_bounds, updated_position, self.dimensions)
-    self.current = tuple(bounded_position)
+    update = self.coordinate_updates[self.actions.index(action)]
+    updated_coords = map(operator.add, self.current_state, update)
+    bounded_coords = map(keep_within_bounds, updated_coords, self.dimensions)
+    self.current_state = tuple(bounded_coords)
--- test_gridworld.py
+++ test_gridworld.py
@@ -4,33 +4,35 @@ from gridworld import GridWorld
 
 class TestGridWorld(unittest.TestCase):
   def setUp(self):
-    self.world = GridWorld((3, 3), (1, 1), (2, 2))
+    self.world = GridWorld(
+      dimensions = (3, 3),
+      start_state = (1, 1),
+      goal_state = (2, 2))
 
-  def test_lists_all_directions(self):
-    self.assertEqual(sorted(self.world.direction_names),
+  def test_lists_all_actions(self):
+    self.assertEqual(sorted(self.world.actions),
       sorted(['left', 'right', 'up', 'down']))
-    self.assertEqual(len(self.world.directions), 4)
 
-  def test_lists_all_positions(self):
-    self.assertEqual(len(self.world.all_positions), 9)
+  def test_lists_all_possible_states(self):
+    self.assertEqual(len(self.world.states), 9)
 
   def test_move(self):
-    self.assertEqual(self.world.current, (1, 1))
+    self.assertEqual(self.world.current_state, (1, 1))
     
-    self.world.move('up')
-    self.assertEqual(self.world.current, (0, 1))
+    self.world.take_action('up')
+    self.assertEqual(self.world.current_state, (0, 1))
 
-    self.world.move('left')
-    self.assertEqual(self.world.current, (0, 0))
+    self.world.take_action('left')
+    self.assertEqual(self.world.current_state, (0, 0))
 
-    self.world.move('down')
-    self.assertEqual(self.world.current, (1, 0))
+    self.world.take_action('down')
+    self.assertEqual(self.world.current_state, (1, 0))
 
-    self.world.move('right')
-    self.assertEqual(self.world.current, (1, 1))
+    self.world.take_action('right')
+    self.assertEqual(self.world.current_state, (1, 1))
 
   def test_moves_on_boundaries(self):
     self.world = GridWorld((1, 1), (0, 0), (0, 0))
-    for direction_name in self.world.direction_names:
-      self.world.move(direction_name)
-      self.assertEqual(self.world.current, (0, 0))
+    for action in self.world.actions:
+      self.world.take_action(action)
+      self.assertEqual(self.world.current_state, (0, 0))
