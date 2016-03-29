7c80f249405d249aa5cc5e0cac44552dfb47157b
Antti Tarvainen <antti.tarvainen@reaktor.com>
2016-03-29 11:29:10 +0300

    Add TD(0) learning rule

---
+++ td0.py
@@ -0,0 +1,11 @@
+class TD0(object):
+  def __init__(self, states, learning_rate):
+    self.__values = dict.fromkeys(states, 0)
+    self.learning_rate = learning_rate
+
+  def __getitem__(self, state):
+    return self.__values[state]
+
+  def learn(self, state, action, reward, new_state):
+    self.__values[state] = (1 - self.learning_rate) * self[state] + \
+        self.learning_rate * (reward + self[new_state])
