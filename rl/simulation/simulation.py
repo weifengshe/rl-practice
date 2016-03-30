549727f2884a150b909846a08e02e9186c41f1bf
Antti Tarvainen <antti.tarvainen@reaktor.com>
2016-03-30 12:26:06 +0300

    Extract method run_step

--- rl/simulation/simulation.py
+++ rl/simulation/simulation.py
@@ -18,15 +18,17 @@ class Simulation(object):
     self.policy.start_episode()
     self.learner.start_episode()
 
-    state = self.environment.current_state
     history = []
-
     while not self.environment.terminated:
-      action = self.policy.choose_action(state)
-      reward, new_state = self.environment.take_action(action)
-      self.learner.learn(state, action, reward, new_state)
-      history.append((state, action, reward))
-      state = new_state
+      history.append(self.run_step())
 
-    history.append((new_state, None, None))
+    history.append((self.environment.current_state, None, None))
     return history
+
+  def run_step(self):
+    assert not self.environment.terminated
+    state = self.environment.current_state
+    action = self.policy.choose_action(state)
+    reward, new_state = self.environment.take_action(action)
+    self.learner.learn(state, action, reward, new_state)
+    return (state, action, reward)
