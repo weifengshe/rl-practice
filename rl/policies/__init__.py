f13a147b95f40da7dc69a0a7d432902d02d120f1
Antti Tarvainen <antti.tarvainen@reaktor.com>
2016-04-08 17:03:00 +0300

    Remove all algorithms except TDAgent

--- rl/policies/__init__.py
+++
@@ -1,3 +0,0 @@
-from greedy_afterstate_policy import GreedyAfterstatePolicy
-from greedy_action_policy import GreedyActionPolicy
-from epsilon_policy import EpsilonPolicy
--- rl/predictors/__init__.py
+++
@@ -1,3 +0,0 @@
-from td0 import TD0
-from td import TD
-from sarsa import Sarsa
